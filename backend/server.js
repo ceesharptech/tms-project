16046c4583fad3e46b1c45b32613cadca9958383
ceesharptech <eniolaamusu6@gmail.com>
2026-03-14 18:16:15 +0100

    Adding new domain URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 5000;
 // ── Global middleware ────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:5173" ||
-      "https://tmsportalng.tech",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
